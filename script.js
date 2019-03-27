54e457c812e8d22e4c02fcf165988e2a149f2c58
paul <paulosullivan22@outlook.com>
2019-03-27 19:10:29 +0000

    adding tagline for r each character

--- script.js
+++ script.js
@@ -13,25 +13,56 @@ const urls = [
 
 const characters = [];
 let lukesFather = '';
-let resultText = document.getElementById('resultText');
 
 Promise.all(urls.map(url =>
   fetch(url).then(resp => resp.json())
-))
+  ))
   .then(results => {
     results.map(results => {
       characters.push(results);
       console.log(results)
     })})
 
+function ifLukesFather(input) {
+  let resultText = document.getElementById('resultText');
+  switch(input) {
+    case "Wilhuff Tarkin":
+      resultText.innerHTML = "Ouch, a receeding hairline";
+      break;
+    case "R2-D2":
+      resultText.innerHTML = "I always thought your acting was a bit robotic";
+      break;
+    case "Darth Vader":
+      resultText.innerHTML = "Well, this is ominous...";
+      break;
+    case "C-3PO":
+      resultText.innerHTML = "Your skin always looked a bit too golden...";
+      break;
+    case "Leia Organa":
+      resultText.innerHTML = "Well apparently she's not only your sister but also your father. You Skywalkers are a weird bunch";
+      break;
+    case "Owen Lars":
+      resultText.innerHTML = "He did raise you on the desert plant of Tatooine so this would make sense";
+      break;
+    case "Beru Whitesun lars":
+      resultText.innerHTML = "Mother maybe, but father might be a bit of a stretch. Still, closer than a few of the other options we have here...";
+      break;
+    case "R5-D4":
+      resultText.innerHTML = "No way! Maybe that's why he was always by your side";
+      break;
+    case "Biggs Darklighter":
+      resultText.innerHTML = "One of your best friends and also your father, a two-in-one!";
+      break;
+    case "Obi-Wan Kenobi":
+      resultText.innerHTML = "One of the most legendary Jedi Masters. You're destined for great things, Luke.";
+      break;
+    default:
+      resultText.innerHTML = "I guess we'll never know";
+  }
+}
+
 document.getElementById('button').addEventListener("click", function() {
   lukesFather = characters[Math.floor((Math.random() * 10))];
   document.getElementById('resultName').innerHTML = (lukesFather.name);
+  ifLukesFather(lukesFather.name);
 })
-
-const fatherBio = (lukesFather) => {
-  console.log(lukesFather);
-  if (lukesFather === "Wilhuff Tarkin") {
-    resultText.innerHTML = "Wow, right up the gut. Wilhuff Tarkin built the Death Star which you popped off. Way to stick it to Dad, Luke."
-  }
-}
